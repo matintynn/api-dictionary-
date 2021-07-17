541c9429fcb1b80a515dfc48ac00b2e8497436a7
matintynn <matintyson0402@gmail.com>
2021-07-16 22:28:24 -0400

    Add the getInput.value = ''  to the js file

--- public/js/main.js
+++ public/js/main.js
@@ -10,30 +10,30 @@ getSearch.addEventListener('submit', (e) => {
     let flag = true;
     let getItem = document.querySelector('h3');
     // remove the old board and replace the new board
-    if(getItem){
+    if (getItem) {
         getDiv.innerHTML = '';
         flag = false;
-    } else if (getInput.value === ''){
+    } else if (getInput.value === '') {
         getDiv.style.display = 'block';
         getDiv.innerHTML = 'Hi! you forgot to type a word';
         getDiv.style.textAlign = 'center';
     }
-    
+
     getDiv.style.display = 'block';
 
     // Fetch the data from the internet
-    async function myDic(){
+    async function myDic() {
         // if the input value is not typed, there is error,
         // so the fetch data from the internet will not work
-        let res = await fetch('https://dictionaryapi.com/api/v3/references/collegiate/json/' + getInput.value +'?key=bf58ade8-47f0-4a7c-adb6-af6ce7221196');
+        let res = await fetch('https://dictionaryapi.com/api/v3/references/collegiate/json/' + getInput.value + '?key=bf58ade8-47f0-4a7c-adb6-af6ce7221196');
         let data = await res.json();
         // access to meta
         let getMeta = data[0];
-        
-        if(getMeta.fl === undefined){
+
+        if (getMeta.fl === undefined) {
             getDiv.innerHTML = 'This word does not exist!';
             getDiv.style.textAlign = 'center';
-        } else{
+        } else {
             getDiv.innerHTML = '';
             getDiv.style.textAlign = '';
 
@@ -56,7 +56,7 @@ getSearch.addEventListener('submit', (e) => {
             let getAudioId = getMeta.hwi.prs[0].sound.audio;
             let getLetter = getAudioId.charAt(0);
 
-            let getAudio = new Audio('https://media.merriam-webster.com/audio/prons/en/us/mp3/' + getLetter + '/' + getAudioId +'.mp3');
+            let getAudio = new Audio('https://media.merriam-webster.com/audio/prons/en/us/mp3/' + getLetter + '/' + getAudioId + '.mp3');
             let speaker = document.querySelector('.speaker');
             speaker.addEventListener('click', () => {
                 getAudio.play();
@@ -71,17 +71,19 @@ getSearch.addEventListener('submit', (e) => {
             })
 
             getDiv.append(getUl);
-        } 
+        }
     }
-   
+
     // call function
     myDic().catch(error => {
         console.log('Error!');
         // check if the input field is empty and the old word is there
-        if((getDiv.style.display = 'block') && (getInput.value === '') && (flag === false)){
+        if ((getDiv.style.display = 'block') && (getInput.value === '') && (flag === false)) {
             getDiv.innerHTML = 'Hi! you forgot to type a word';
             getDiv.style.textAlign = 'center';
             flag = true;
         }
     });
+    // to reset the value of the input field
+    getInput.value = "";
 });
